d694c88a8524847a503cf68a03b5d50a3e0facb2
Peter Trizuliak <peter.trizuliak@gmail.com>
2016-11-26 22:36:36 +0000

    5 Functions refactored

--- pet_shop.rb
+++ pet_shop.rb
@@ -24,23 +24,29 @@ end
 
 def pets_by_breed(pets_shop, breed)
   asked_breed = []
-  for i in pets_shop[:pets]
-    asked_breed.push(i[:breed]) if i[:breed] == breed
-  end
+  # for each in pets_shop[:pets]
+  #   asked_breed.push(each) if i[:breed] == breed
+  # end
+  # #1 REFACTORED FROM FOR LOOP TO EACH METHOD
+  pets_shop[:pets].each{ |i| asked_breed.push(i) if i[:breed] == breed }
   return asked_breed
 end
 
 def find_pet_by_name(pets_shop, pet_name)
-  for i in pets_shop[:pets]
-    return i if i[:name] == pet_name
-  end
+  # for i in pets_shop[:pets]
+  #   return i if i[:name] == pet_name
+  # end
+  # #2 REFACTORED FROM FOR LOOP TO EACH METHOD
+  pets_shop[:pets].each{ |i| return i if i[:name] == pet_name }
   return nil
 end
 
 def remove_pet_by_name(pets_shop, pet_name)
-  for i in pets_shop[:pets]
-    pets_shop[:pets].delete(i) if i[:name] == pet_name
-  end
+  # for i in pets_shop[:pets]
+  #   pets_shop[:pets].delete(i) if i[:name] == pet_name
+  # end
+  # #3 REFACTORED FROM FOR LOOP TO EACH METHOD
+  pets_shop[:pets].each { |i| pets_shop[:pets].delete(i) if i[:name] == pet_name }
 end
 
 def add_pet_to_stock(pets_shop, new_pet)
@@ -56,24 +62,38 @@ def add_pet_to_customer(customer, new_pet)
 end
 
 def customer_can_afford_pet(customer, new_pet)
-  if customer[:cash] >= new_pet[:price]
-    return true
-  else
-    return false
-  end
+  # if customer[:cash] >= new_pet[:price]
+  #   return true
+  # else
+  #   return false
+  # end
+  # #4 REFACTORED FROM IF ELSE STATEMENT TO TERNARY 
+  return customer[:cash] >= new_pet[:price] ? true : false
 end
 
 def sell_pet_to_customer(pets_shop, pet, customer)
-  if pet != nil
-    if customer_can_afford_pet(customer, pet)
-      customer[:cash] -= pet[:price]
-      add_or_remove_cash(pets_shop, pet[:price])
-      add_pet_to_customer(customer, pet)
-      increase_pets_sold(pets_shop, 1)
-    else
-      puts "Sorry, we cannot sell you the animal, you have insufficient funds."
-    end
+  # if pet != nil
+  #   if customer_can_afford_pet(customer, pet)
+  #     customer[:cash] -= pet[:price]
+  #     add_or_remove_cash(pets_shop, pet[:price])
+  #     add_pet_to_customer(customer, pet)
+  #     increase_pets_sold(pets_shop, 1)
+  #   else
+  #     puts "Sorry, we cannot sell you the animal, you have insufficient funds."
+  #   end
+  # else
+  #   puts "Sorry, no pet like that in our stock"
+  # end
+  # #5 REFACTORED FROM 2 IF ELSE STATEMENTS TO 1 IF ELSIF ELSE STATEMENT
+  if pet == nil
+    puts "Sorry, no pet like that in our stock."
+  elsif customer_can_afford_pet(customer, pet)
+    customer[:cash] -= pet[:price]
+    add_or_remove_cash(pets_shop, pet[:price])
+    pets_shop[:pets].delete_if{ |i| i == pet}
+    add_pet_to_customer(customer, pet)
+    increase_pets_sold(pets_shop, 1)
   else
-    puts "Sorry, no pet like that in our stock"
+    puts "Sorry, we cannot sell you the animal, you have insufficient funds."
   end
 end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -179,7 +179,7 @@ class TestPetShop < Minitest::Test
   def test_sell_pet_to_customer__pet_found
     customer = @customers[0]
     pet = find_pet_by_name(@pet_shop,"Arthur")
-
+    
     sell_pet_to_customer(@pet_shop, pet, customer)
 
     assert_equal(1, customer_pet_count(customer))
@@ -190,7 +190,7 @@ class TestPetShop < Minitest::Test
   def test_sell_pet_to_customer__pet_not_found
     customer = @customers[0]
     pet = find_pet_by_name(@pet_shop,"Dave")
-
+    
     sell_pet_to_customer(@pet_shop, pet, customer)
 
     assert_equal(0, customer_pet_count(customer))
@@ -201,7 +201,7 @@ class TestPetShop < Minitest::Test
   def test_sell_pet_to_customer__insufficient_funds
     customer = @customers[1]
     pet = find_pet_by_name(@pet_shop,"Arthur")
-
+    
     sell_pet_to_customer(@pet_shop, pet, customer)
 
     assert_equal(0, customer_pet_count(customer))
